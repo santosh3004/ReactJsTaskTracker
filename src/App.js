dc0931d949ebb7d6f6f91bce473242b6fa08b640
santosh3004 <msantoaubedi12@gmail.com>
2021-11-29 14:00:20 +0545

    AfterPropTypes
    This is after learning the props types and styling html elements in js.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+
 
 function App() {
+  const name='Santosh Subedi'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p class="text-dark">
-          Hello Worldqweqweasdasdasd
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header/>
     </div>
   );
 }
