7ea03fb3b05f36dc6ac6b5c04bf9eff5104cd75f
santosh3004 <msantoaubedi12@gmail.com>
2021-11-29 14:22:48 +0545

    About Button
    Detail on button creation and passing the props(arguments/parameters) to work on and create a function to make it do a event.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Header from './components/Header'
 
 
 function App() {
-  const name='Santosh Subedi'
   return (
     <div className="container">
       <Header/>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 const Header=({title})=>{
+  const onClick = () =>{
+    console.log('Hello Peter')
+  }
   return(
     <header className='header'>
       <h1>{title}</h1>
-      <button className='btn'>Add</button>
+      <Button color='green' text='Add' onClick={onClick}/>
     </header>
   )
 }
