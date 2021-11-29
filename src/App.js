c5a35ef84d9c897f1f83e4580cf21f74febe4591
santosh3004 <msantoaubedi12@gmail.com>
2021-11-29 15:06:53 +0545

    After Filtering Array(useStaet use)
    filtering data from array after clicking a cross icon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import Header from './components/Header'
+import Tasks from './components/Tasks';
+import {useState} from 'react'
 
 
 function App() {
+  const [tasks,setTasks]=useState([{
+    id:1,
+    text:'First Task Add Web App',
+    day:'Nov 29 2021',
+    reminder:true
+  }])
+  //Delete task
+  const deleteTask=(id)=>{
+    setTasks(tasks.filter((task)=> task.id!==id))
+  }
   return (
     <div className="container">
       <Header/>
+      {tasks.length>0?<Tasks tasks={tasks} onDelete={deleteTask}/>:''}
     </div>
   );
 }
